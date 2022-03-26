f5c7c4beb992b5ff8b52d3d2dac839c35c3a379c
Arvin-2005 <102425221+Arvin-2005@users.noreply.github.com>
2022-03-26 20:09:15 +0430

    Add files via upload

---
+++ 5x5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _5x5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("{0,5}", i * 1);
+                Console.Write("{0,5}", i * 2);
+                Console.Write("{0,5}", i * 3);
+                Console.Write("{0,5}", i * 4);
+                Console.Write("{0,5}", i * 5);
+                Console.WriteLine();
+            }
+        }
+    }
+}
